d5a459de7a1a284dead572de327e797fe5bd1a09
Jisoo Kim <wlth249@gmail.com>
2026-07-01 13:45:00 +0200

    dma init start done

--- drivers/Inc/stm32f407xx_dma_driver.h
+++ drivers/Inc/stm32f407xx_dma_driver.h
@@ -3,93 +3,210 @@
 
 #include "stm32f407xx.h"
 
+#define DMA_OK 0
+#define DMA_NG -1
+
+/*
+    DMA timeout
+*/
+#define DMA_TIMEOUT_ERROR       -1
+
+/*
+    RESET : before init
+    READY : init done ready to start
+    BUSY : dma on going
+    COMPLETE : trasnfer complete 
+    ERROR : error
+*/
+typedef enum
+{
+    DMA_STATE_RESET = 0,
+    DMA_STATE_READY,
+    DMA_STATE_BUSY,
+    DMA_STATE_COMPLETE,
+    DMA_STATE_ERROR
+} DMA_State_t;
+
+typedef enum
+{
+    DMA_CONFIG_OK = 0,
+    DMA_CONFIG_INVALID_PARAM,
+    DMA_CONFIG_INVALID_STREAM,
+    DMA_CONFIG_INVALID_CHANNEL,
+    DMA_CONFIG_INVALID_REQUEST,
+} DMA_ConfigStatus_t;
+
+
 // DMA MODE ENABLE
 #define DMA_DMDIS_BIT 2
 
-#define DMA_MODE_DIRECT (0 << DMA_DMDIS_BIT)
-#define DMA_MODE_FIFO   (1 << DMA_DMDIS_BIT)
+#define DMA_MODE_DIRECT_BIT (0 << DMA_DMDIS_BIT)
+#define DMA_MODE_FIFO_BIT   (1 << DMA_DMDIS_BIT)
+
+typedef enum
+{
+    DMA_MODE_DIRECT,
+    DMA_MODE_FIFO
+}DMA_Mode_t;
 
 /*
 	DMA FIFO Threshold selection
 */
-#define DMA_FIFO_THRES_1_4   0
-#define DMA_FIFO_THRES_2_4   1
-#define DMA_FIFO_THRES_3_4   2
-#define DMA_FIFO_THRES_FULL  3
+#define DMA_FIFO_THRES_1_4_BIT   0
+#define DMA_FIFO_THRES_2_4_BIT   1
+#define DMA_FIFO_THRES_3_4_BIT   2
+#define DMA_FIFO_THRES_FULL_BIT  3
 
+typedef enum
+{
+    DMA_FIFO_THRES_1_4,
+    DMA_FIFO_THRES_2_4,
+    DMA_FIFO_THRES_3_4,
+    DMA_FIFO_THRES_FULL
+}DMA_FIFO_t;
 
 // DMA channael select
-#define DMA_CHANNEL0 (0 << 25)
-#define DMA_CHANNEL1 (1 << 25)
-#define DMA_CHANNEL2 (2 << 25)
-#define DMA_CHANNEL3 (3 << 25)
-#define DMA_CHANNEL4 (4 << 25)
-#define DMA_CHANNEL5 (5 << 25)
-#define DMA_CHANNEL6 (6 << 25)
-#define DMA_CHANNEL7 (7 << 25)
+#define DMA_CH0_BIT (0 << 25)
+#define DMA_CH1_BIT (1 << 25)
+#define DMA_CH2_BIT (2 << 25)
+#define DMA_CH3_BIT (3 << 25)
+#define DMA_CH4_BIT (4 << 25)
+#define DMA_CH5_BIT (5 << 25)
+#define DMA_CH6_BIT (6 << 25)
+#define DMA_CH7_BIT (7 << 25)
+
+typedef enum
+{
+    DMA_CH0,
+    DMA_CH1,
+    DMA_CH2,
+    DMA_CH3,
+    DMA_CH4,
+    DMA_CH5,
+    DMA_CH6,
+    DMA_CH7
+}DMA_Channel_t;
 
 // DMA STREAM EN or DI
-#define DMA_STREAM_ENABLE  (1 << 0)
-#define DMA_STREAM_DISABLE (0 << 0)
+#define DMA_STREAM_ENABLE_BIT  (1 << 0)
+#define DMA_STREAM_DISABLE_BIT (0 << 0)
+
+typedef enum
+{
+    DMA_STREAM_ENABLE,
+    DMA_STREAM_DISABLE
+}DMA_StreamState_t;
 
 /*
     DMA Memory brust transfer config
     - These bits are protected and can be written only if EN is ‘0’
     - In direct mode, these bits are forced to 0x0 by hardware as soon as bit EN= '1'.
 */
-#define DMA_MBURST_SINGLE   (0 << 23)
-#define DMA_MBURST_INCR4    (1 << 23)
-#define DMA_MBURST_INCR8    (2 << 23)
-#define DMA_MBURST_INCR16   (3 << 23)
+#define DMA_MBURST_SINGLE_BIT   (0 << 23)
+#define DMA_MBURST_INCR4_BIT    (1 << 23)
+#define DMA_MBURST_INCR8_BIT    (2 << 23)
+#define DMA_MBURST_INCR16_BIT   (3 << 23)
+
+typedef enum
+{
+    DMA_MBURST_SINGLE,
+    DMA_MBURST_INCR4,
+    DMA_MBURST_INCR8,
+    DMA_MBURST_INCR16
+}DMA_MBurst_t;
 
 /*
     DMA Pheripheral burst transfer config
     - These bits are protected and can be written only if EN is ‘0’
     - In direct mode, these bits are forced to 0x0 by hardware.
 */
-#define DMA_PBURST_SINGLE   (0 << 21)
-#define DMA_PBURST_INCR4    (1 << 21)
-#define DMA_PBURST_INCR8    (2 << 21)
-#define DMA_PBURST_INCR16   (3 << 21)
+#define DMA_PBURST_SINGLE_BIT   (0 << 21)
+#define DMA_PBURST_INCR4_BIT    (1 << 21)
+#define DMA_PBURST_INCR8_BIT    (2 << 21)
+#define DMA_PBURST_INCR16_BIT   (3 << 21)
 
-/*
-    DMA timeout
-*/
-#define DMA_TIMEOUT_ERROR       -1
-#define DMA_TIME_IN             100000U
+typedef enum
+{
+    DMA_PBURST_SINGLE,
+    DMA_PBURST_INCR4,
+    DMA_PBURST_INCR8,
+    DMA_PBURST_INCR16
+}DMA_PBurst_t;
 
 /*
     DMA SxCR CIRC Circular mode
 */
-#define DMA_CIRC_ENABLE (1 << 8)
-#define DMA_CIRC_DISABLE (0 << 8)
+#define DMA_CIRC_ENABLE_BIT (1 << 8)
+#define DMA_CIRC_DISABLE_BIT (0 << 8)
+
+typedef enum
+{
+    DMA_CIRC_ENABLE,
+    DMA_CIRC_DISABLE
+}DMA_Circ_t;
+
 
 /*
     DMA Transfer Dir
 */
-#define DMA_PERIPHERAL_TO_MEMORY (0 << 6)
-#define DMA_MEMORY_TO_PERIPHERAL (1 << 6)
-#define DMA_MEMORY_TO_MEMORY     (2 << 6)
+#define DMA_DIR_P2M_BIT (0 << 6)
+#define DMA_DIR_M2P_BIT (1 << 6)
+#define DMA_DIR_M2M_BIT (2 << 6)
+
+typedef enum
+{
+    DMA_DIR_P2M,
+    DMA_DIR_M2P,
+    DMA_DIR_M2M
+} DMA_Direction_t;
 
 /*
     DMA Peripheral Increment Mode
 */
-#define DMA_MINC_ENABLE (1 << 10)
-#define DMA_MINC_DISABLE (0 << 10)
+#define DMA_MINC_ENABLE_BIT (1 << 10)
+#define DMA_MINC_DISABLE_BIT (0 << 10)
 
+typedef enum
+{
+    DMA_MINC_ENABLE,
+    DMA_MINC_DISABLE
+}DMA_MInc_t;
 /*
     DMA Memory Increment Mode
 */
-#define DMA_PINC_ENABLE (1 << 9)
-#define DMA_PINC_DISABLE (0 << 9)
+
+#define DMA_PINC_ENABLE_BIT (1 << 9)
+#define DMA_PINC_DISABLE_BIT (0 << 9)
+
+typedef enum
+{
+    DMA_PINC_ENABLE,
+    DMA_PINC_DISABLE
+}DMA_PInc_t;
 
 /*
     DMA Priority
 */
-#define DMA_PRIORITY_LOW (0 << 16)
-#define DMA_PRIORITY_MEDIUM (1 << 16)
-#define DMA_PRIORITY_HIGH (2 << 16)
-#define DMA_PRIORITY_VERY_HIGH (3 << 16)
+#define DMA_PRIORITY_LOW_BIT (0 << 16)
+#define DMA_PRIORITY_MEDIUM_BIT (1 << 16)
+#define DMA_PRIORITY_HIGH_BIT (2 << 16)
+#define DMA_PRIORITY_VERY_HIGH_BIT (3 << 16)
+
+typedef enum
+{
+    DMA_PRIORITY_LOW,
+    DMA_PRIORITY_MEDIUM,
+    DMA_PRIORITY_HIGH,
+    DMA_PRIORITY_VERY_HIGH
+}DMA_Priority_t;
+
+
+typedef enum
+{
+    DMA_DATASIZE_8BIT,
+    DMA_DATASIZE_16BIT,
+    DMA_DATASIZE_32BIT
+}DMA_DataSize_t;
 
 /*
     DMA MEM DATASIZE  
@@ -105,20 +222,10 @@
 #define DMA_PERI_DATASIZE_16BIT (1 << 11)
 #define DMA_PERI_DATASIZE_32BIT (2 << 11)
 
-
 /*
     Macro get stream number
 */
 #define GET_DMA_STREAM_NUM(pDMAx, pDMAStreamx) (uint8_t)((pDMAStreamx - (pDMAx->stream)))
-
-typedef enum
-{
-    DMA_STATE_IDLE,
-    DMA_STATE_READY,
-    DMA_STATE_BUSY,
-    DMA_STATE_DONE,
-    DMA_STATE_ERROR
-} DMA_State_t;
 
 typedef struct
 {
@@ -162,27 +269,6 @@ typedef enum
     DMA_STREAM7
 } DMA_StreamNum_t;
 
-typedef enum
-{
-    DMA_CH0 = 0,
-    DMA_CH1,
-    DMA_CH2,
-    DMA_CH3,
-    DMA_CH4,
-    DMA_CH5,
-    DMA_CH6,
-    DMA_CH7
-} DMA_Channel_t;
-
-typedef enum
-{
-    DMA_MAP_OK = 0,
-    DMA_MAP_INVALID_STREAM,
-    DMA_MAP_INVALID_CHANNEL,
-    DMA_MAP_INVALID_COMBINATION,
-    DMA_MAP_UNSUPPORTED_MODE
-} DMA_MapStatus_t;
-
 typedef struct
 {
     DMA_Reg_t *pDMAx;
@@ -223,23 +309,22 @@ static const DMA_Map_t dma_map[DMA_REQ_TABLE_MAX] =
 
 typedef struct
 {
-    uint32_t TransferDir;
-    uint32_t PeriphInc;
-    uint32_t MemInc;
-    uint32_t PeriDataSize;
-    uint32_t MemDataSize;
-    uint32_t Priority;
-    uint32_t CircularMode;
-    uint32_t FIFO_Or_DirectMode;
-    uint32_t FIFOThreshold;
-    uint32_t MemBurst;
-    uint32_t PerBurst;
+    DMA_Direction_t TransferDir;
+    DMA_PInc_t PeriphInc;
+    DMA_MInc_t MemInc;
+    DMA_DataSize_t PeriDataSize;
+    DMA_DataSize_t MemDataSize;
+    DMA_Priority_t Priority;
+    DMA_Circ_t CircularMode;
+    DMA_Mode_t FIFO_Or_DirectMode;
+    DMA_FIFO_t FIFOThreshold;
+    DMA_MBurst_t MemBurst;
+    DMA_PBurst_t PerBurst;
 }DMA_Config_t;
 
 typedef struct
 {
   DMA_Reg_t *pDMAx;
-  const DMA_Map_t *map;
   DMA_Channel_t channel;
   uint8_t streamNum;
   uintptr_t peripheral; //peirpheral addr.
@@ -250,7 +335,6 @@ typedef struct
 } DMA_Handle_t;
 
 void DMA_PeriClockControl(DMA_Reg_t *pDMAx, uint8_t EnOrDi);
-DMA_MapStatus_t DMA_reqMapInit(DMA_Handle_t *pDMAHandler, DMA_Request_t req);
-void DMA_Init(DMA_Handle_t *pDMAHandler);
+int32_t DMA_Init(DMA_Handle_t *pDMAHandler);
 void DMA_DeInit(DMA_Stream_Reg_t *pDMAx);
 #endif
--- drivers/Src/stm32f407xx_dma_driver.c
+++ drivers/Src/stm32f407xx_dma_driver.c
@@ -35,68 +35,274 @@ void DMA_PeriClockControl(DMA_Reg_t *pDMAx, uint8_t EnOrDi)
 /*
     DMA MAP init 
 */
-DMA_MapStatus_t DMA_reqMapInit(DMA_Handle_t *pDMAHandler, DMA_Request_t req)
+static int32_t DMA_mapRequest(DMA_Handle_t *pDMAHandler, DMA_Request_t req)
 {
-    if (pDMAHandler == NULL) return DMA_MAP_INVALID_COMBINATION;
-    if (req >= DMA_REQ_TABLE_MAX) return DMA_MAP_INVALID_COMBINATION;
+    if (pDMAHandler == NULL) return -DMA_CONFIG_INVALID_PARAM;
+    if (req >= DMA_REQ_TABLE_MAX) return -DMA_CONFIG_INVALID_REQUEST;
     
     const DMA_Map_t *map = &dma_map[req];
-    
-    pDMAHandler->map = map;
+
     pDMAHandler->streamNum = map->stream;
     pDMAHandler->channel = map->channel;
     pDMAHandler->pDMAx = map->pDMAx;
     
-    return DMA_MAP_OK;
+    return DMA_OK;
 }
 
+static int32_t DMA_waitDisable(DMA_Handle_t *handler)
+{
+    uint32_t timeout = 100000;
 
+    DMA_Stream_Reg_t *stream = &handler->pDMAx->stream[handler->streamNum];
+    while (stream->CR & (1 << 0))
+    {
+        if (--timeout == 0)
+            return DMA_TIMEOUT_ERROR;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_clearFlags(DMA_Handle_t *handler)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    
+    const DMA_FlagMap_t *fmap = &dma_flag_map[handler->streamNum];
+    uint32_t mask = (0x3D << fmap->offset);
+
+    if (fmap->is_high == 0)
+        handler->pDMAx->LIFCR = mask;
+    else
+        handler->pDMAx->HIFCR = mask;
+
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_dirConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.TransferDir)
+    {
+        case DMA_DIR_P2M:
+            *temp |= DMA_DIR_P2M_BIT;
+            break;
+        case DMA_DIR_M2P:
+            *temp |= DMA_DIR_M2P_BIT;
+            break;
+        case DMA_DIR_M2M:
+            *temp |= DMA_DIR_M2M_BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_MIncConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.MemInc)
+    {
+        case DMA_MINC_ENABLE:
+            *temp |= DMA_MINC_ENABLE_BIT;
+            break;
+        case DMA_MINC_DISABLE:
+            *temp |= DMA_MINC_DISABLE_BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_PIncConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.PeriphInc)
+    {
+        case DMA_PINC_ENABLE:
+            *temp |= DMA_PINC_ENABLE_BIT;
+            break;
+        case DMA_PINC_DISABLE:
+            *temp |= DMA_PINC_DISABLE_BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_PeriDataSizeConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.PeriDataSize)
+    {
+        case DMA_DATASIZE_8BIT:
+            *temp |= DMA_PERI_DATASIZE_8BIT;
+            break;
+        case DMA_DATASIZE_16BIT:
+            *temp |= DMA_PERI_DATASIZE_16BIT;
+            break;
+        case DMA_DATASIZE_32BIT:
+            *temp |= DMA_PERI_DATASIZE_32BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_MemDataSizeConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.MemDataSize)
+    {
+        case DMA_DATASIZE_8BIT:
+            *temp |= DMA_MEM_DATASIZE_8BIT;
+            break;
+        case DMA_DATASIZE_16BIT:
+            *temp |= DMA_MEM_DATASIZE_16BIT;
+            break;
+        case DMA_DATASIZE_32BIT:
+            *temp |= DMA_MEM_DATASIZE_32BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_PriorityConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.Priority)
+    {
+        case DMA_PRIORITY_LOW:
+            *temp |= DMA_PRIORITY_LOW_BIT;
+            break;
+        case DMA_PRIORITY_MEDIUM:
+            *temp |= DMA_PRIORITY_MEDIUM_BIT;
+            break;
+        case DMA_PRIORITY_HIGH:
+            *temp |= DMA_PRIORITY_HIGH_BIT;
+            break;
+        case DMA_PRIORITY_VERY_HIGH:
+            *temp |= DMA_PRIORITY_VERY_HIGH_BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_CircularModeConfig(DMA_Handle_t *handler, uint32_t *temp)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.CircularMode)
+    {
+        case DMA_CIRC_ENABLE:
+            *temp |= DMA_CIRC_ENABLE_BIT;
+            break;
+        case DMA_CIRC_DISABLE:
+            *temp |= DMA_CIRC_DISABLE_BIT;
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
+
+static int32_t DMA_CR_ModeConfig(DMA_Handle_t *handler, uint32_t *fcr)
+{
+    if (handler == NULL) return DMA_CONFIG_INVALID_PARAM;
+    switch(handler->config.FIFO_Or_DirectMode)
+    {
+        case DMA_MODE_DIRECT:
+            *fcr |= DMA_MODE_DIRECT_BIT;
+            break;
+        case DMA_MODE_FIFO:
+            *fcr |= DMA_MODE_FIFO_BIT;
+            switch(handler->config.FIFOThreshold)
+            {
+                case DMA_FIFO_THRES_1_4:
+                    *fcr |= DMA_FIFO_THRES_1_4_BIT;
+                    break;
+                case DMA_FIFO_THRES_2_4:
+                    *fcr |= DMA_FIFO_THRES_2_4_BIT;
+                    break;
+                case DMA_FIFO_THRES_3_4:
+                    *fcr |= DMA_FIFO_THRES_3_4_BIT;
+                    break;
+                case DMA_FIFO_THRES_FULL:
+                    *fcr |= DMA_FIFO_THRES_FULL_BIT;
+                    break;
+                default:
+                    return -DMA_CONFIG_INVALID_PARAM;
+            }
+            break;
+        default:
+            return -DMA_CONFIG_INVALID_PARAM;
+    }
+    return DMA_OK;
+}
 /*
     DMA Initialization
     @param1 : DMA Handle pointer
     @note: The caller must set DMA_Config_t before calling this
 */
-void DMA_Init(DMA_Handle_t *pDMAHandler)
+int32_t DMA_Init(DMA_Handle_t *pDMAHandler)
 {
-    uint32_t timeout = 100000;
-    DMA_Stream_Reg_t *stream = &pDMAHandler->pDMAx->stream[pDMAHandler->streamNum];
-    stream->CR &= ~(1 << 0);
-    while(stream->CR & (1 << 0))
-    {
-        if(--timeout == 0)
-            break;
-            //return DMA_TIMEOUT_ERROR;
-    }
-    const DMA_FlagMap_t *fmap = &dma_flag_map[pDMAHandler->streamNum];
-    uint32_t clear_mask = (0x3D << fmap->offset);
-    if (fmap->is_high == 0)
-    {
-        pDMAHandler->pDMAx->LIFCR = clear_mask;
-    }
-    else
-    {
-        pDMAHandler->pDMAx->HIFCR = clear_mask;
-    }
-    // Config CR reg
+    if (pDMAHandler == NULL)
+        return DMA_CONFIG_INVALID_PARAM;
+    if (DMA_waitDisable(pDMAHandler) != DMA_OK)
+        return DMA_NG;
+    if (DMA_clearFlags(pDMAHandler) != DMA_OK)
+        return DMA_NG;
     uint32_t temp = 0;
     temp |= pDMAHandler->channel;
-    temp |= DMA_PERIPHERAL_TO_MEMORY;
-    temp |= DMA_MINC_ENABLE;
-    temp |= DMA_MEM_DATASIZE_8BIT;
-    temp |= DMA_PERI_DATASIZE_8BIT;
-    temp |= DMA_PRIORITY_LOW;
-    temp |= DMA_CIRC_ENABLE;
+    if (DMA_CR_dirConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_MIncConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_PIncConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_PeriDataSizeConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_MemDataSizeConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_PriorityConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    if (DMA_CR_CircularModeConfig(pDMAHandler, &temp) != DMA_OK)
+        return DMA_NG;
+    DMA_Stream_Reg_t *stream = &pDMAHandler->pDMAx->stream[pDMAHandler->streamNum];
+    temp &= ~DMA_STREAM_ENABLE_BIT;
     stream->CR = temp;
-
     uint32_t fcr = 0;
-    fcr |= DMA_MODE_DIRECT;
+    if (DMA_CR_ModeConfig(pDMAHandler, &fcr) != DMA_OK)
+        return DMA_NG;
     stream->FCR = fcr;
-
     stream->PAR = pDMAHandler->peripheral;
     stream->M0AR = pDMAHandler->mem;
     stream->NDTR = pDMAHandler->length;
- 
     pDMAHandler->state = DMA_STATE_READY;
+    return DMA_OK;
+}
+
+/*
+    @brief : DMA start
+    @param1 : DMA Handle pointer
+    @note : if hardware is already enabled, start is rejected.
+*/
+int32_t DMA_start(DMA_Handle_t *handler)
+{
+    if (handler == NULL) return DMA_NG;
+    if (handler->state != DMA_STATE_READY) return DMA_NG;
+
+    if (handler->pDMAx->stream[handler->streamNum].CR & DMA_STREAM_ENABLE_BIT)
+        return DMA_NG;
+
+    handler->pDMAx->stream[handler->streamNum].CR |= DMA_STREAM_ENABLE_BIT;
+    handler->state = DMA_STATE_BUSY;
+    return DMA_OK;
 }
 
 void DMA_DeInit(DMA_Stream_Reg_t *pDCMAx)
